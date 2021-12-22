74676163a3a02229bc19d827589fc9592665618e
Lee Kyeong Ryong <kryongleesec@gmail.com>
2021-12-22 21:46:17 +0900

    sendmqtt.c 2

---
+++ rpidevsync/src/sendmqtt.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <time.h>
+
+#include <MQTTAsync.h>
+#include <sendmqtt.h>
