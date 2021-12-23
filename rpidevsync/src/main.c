4221de9d4932cdd5fcf1bcb60cae23616eced630
orangecalculator <kryongleesec@gmail.com>
2021-12-23 11:37:53 +0900

    main for ecg2mqtt 1

--- rpidevsync/src/main.c
+++ rpidevsync/src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <string.h>
 #include <unistd.h>
 #include <getopt.h>
@@ -10,3 +11,15 @@
 #include <readecg.h>
 #include <sendmqtt.h>
 
+/*
+ * krlee@sysi9:~/infotracking/ecgprocess/rpidevsync$ python3 ../../ecgdisplay/samples/ecgtomqttsim.py --help
+ * usage: ecgtomqttsim.py [-h] [--sanitize] [-s SEED] [--noise NOISE]
+ *                        [-r RESOLUTION] [--resolution-stderr RESOLUTION_STDERR]
+ *                        [-i CLIENT_ID] [-H HOST] [-p PORT] [-t TOPIC]
+ *                        [-u USERNAME] [-P PASSWORD] [--tls] [--cacert CACERT]
+ *                        [--cert CERT] [--key KEY] [--insecure]
+ *                        [--normal ACTION] [--abnormal ACTION]
+ *                        [--normal-cycle ACTION] [--abnormal-cycle ACTION]
+ * 
+ * optional arguments:
+ *   -h, --help            show this help message and exit
