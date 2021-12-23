64eefe019c908e250cff88ea34b928bf43d88abf
orangecalculator <kryongleesec@gmail.com>
2021-12-23 13:56:38 +0900

    main for ecg2mqtt 29

--- rpidevsync/src/main.c
+++ rpidevsync/src/main.c
@@ -1,12 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
 
 #include <string.h>
 #include <unistd.h>
-#include <getopt.h>
-#include <errno.h>
 #include <pthread.h>
-#include <signal.h>
 
 #include <readecg.h>
 #include <sendmqtt.h>
