f4d6a38e5b01eb794db59c544edbbba94110e629
Lee Kyeong Ryong <kryongleesec@gmail.com>
2021-12-23 16:12:11 +0900

    readecg.c

--- rpidevsync/src/readecg.c
+++ rpidevsync/src/readecg.c
@@ -16,6 +16,7 @@
 // forward declaration
 int mcp3004_open(const char * devname, int spimode, int lsbfirst, unsigned bits_per_word, uint32_t max_speed_hz);
 int mcp3004_readvalue(int fd, unsigned channel);
+uint64_t mcp3004_value_to_voltage(uint32_t voltage);
 int mcp3004_close(int fd);
 
 int mcp3004_open(const char * devname, int spimode, int lsbfirst, unsigned bits_per_word, uint32_t max_speed_hz){
@@ -137,6 +138,10 @@ int mcp3004_readvalue(int fd, unsigned channel){
 	return voltage;
 
 #undef	TRANSFER_LEN
+}
+
+uint64_t mcp3004_value_to_voltage(uint32_t voltage){
+	return (uint64_t) ((double)voltage / (1 << 10) * 3.3 * 1e6);
 }
 
 int mcp3004_close(int fd){
