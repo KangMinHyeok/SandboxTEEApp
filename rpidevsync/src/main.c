a005a532a4ee6198d8c678685b8e8b214cb04c1c
orangecalculator <kryongleesec@gmail.com>
2021-12-25 10:02:00 +0900

    main.c argument parsing 10

--- rpidevsync/src/main.c
+++ rpidevsync/src/main.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,55 +10,6 @@
 #include <readecg.h>
 #include <sendmqtt.h>
 
-/*
- * krlee@sysi9:~/infotracking/ecgprocess/rpidevsync$ python3 ../../ecgdisplay/samples/ecgtomqttsim.py --help
- * usage: ecgtomqttsim.py [-h] [--sanitize] [-s SEED] [--noise NOISE]
- *                        [-r RESOLUTION] [--resolution-stderr RESOLUTION_STDERR]
- *                        [-i CLIENT_ID] [-H HOST] [-p PORT] [-t TOPIC]
- *                        [-u USERNAME] [-P PASSWORD] [--tls] [--cacert CACERT]
- *                        [--cert CERT] [--key KEY] [--insecure]
- *                        [--normal ACTION] [--abnormal ACTION]
- *                        [--normal-cycle ACTION] [--abnormal-cycle ACTION]
- * 
- * optional arguments:
- *   -h, --help            show this help message and exit
- *   --sanitize            sanitize behavior of program
- *   -s SEED, --seed SEED  seed for generating random number
- *   --noise NOISE         magnitude of noise of ecg signal
- *   -r RESOLUTION, --resolution RESOLUTION
- *                         time interval between each sample
- *   --resolution-stderr RESOLUTION_STDERR
- *                         std error of time interval between each sample
- *   -i CLIENT_ID, --client-id CLIENT_ID
- *                         id to use for this client (default: sampleclient
- *                         appended with random number)
- *   -H HOST, --host HOST  mqtt host to connect to (default: localhost)
- *   -p PORT, --port PORT  network port to connect to (default: 1883)
- *   -t TOPIC, --topic TOPIC
- *                         mqtt topic to publish to (default: hello)
- *   -u USERNAME, --username USERNAME
- *                         provide a username
- *   -P PASSWORD, --password PASSWORD
- *                         provide a password
- *   --tls                 enable tls
- *   --cacert CACERT       path to a file containing trusted CA certificates to
- *                         enable encrypted communication
- *   --cert CERT           client certificate for authentication if required by
- *                         server
- *   --key KEY             client private key for authentication if required by
- *                         server
- *   --insecure            disable server certificate verification
- *   --normal ACTION       send normal ecg signal (unit: ms)
- *   --abnormal ACTION     send abnormal ecg signal (unit: ms)
- *   --normal-cycle ACTION
- *                         send normal ecg signal (unit: cycle)
- *   --abnormal-cycle ACTION
- *                         send abnormal ecg signal (unit: cycle)
- */
-
-#define ADDRESS     "ssl://147.46.244.130:8883"
-#define CLIENTID    "RaspberryPiAlpha"
-#define USERNAME    "demo"
 #define PASSWORD    "guest"
 #define TOPIC       "hello"
 //#define PAYLOAD     "Msg from RaspberryPiAlpha"
